adbba3282d21576481c6e3aeeba6993d1e967932
g3-tou <munira.sangaaf@gmail.com>
2024-09-25 22:06:35 +1000

    implemented moving around the box

--- Assets/Scripts/PacStuMove.cs
+++ Assets/Scripts/PacStuMove.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class PacStuMove : MonoBehaviour
 {
+    private Vector2[] positions;
+    private Vector2 startPos;
+    private Vector2 targetPos;
+
+    private float dist = 1f;
+    private int currentInd = 0;
+    private float t = 0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        positions = new Vector2[]{
+            new Vector2(0, 0),
+            new Vector2(-5, 0),
+            new Vector2(-5, 4),
+            new Vector2(0, 4)
+        };
+        startPos = positions[0];
+        targetPos = positions[currentInd];
     }
 
     // Update is called once per frame
     void Update()
     {
+        t += Time.deltaTime / dist;
+        transform.position = Vector2.Lerp(startPos, targetPos, t);
+
+        if(t >= 1f){
+            t = 0f;
+            startPos = transform.position;
+            currentInd = (currentInd + 1) % positions.Length;
+            targetPos = positions[currentInd];
+        }
         
     }
 }
