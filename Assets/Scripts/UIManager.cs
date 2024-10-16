114da9ad88573ef1e2b363f0b5698d7dca5d0488
g3-tou <munira.sangaaf@gmail.com>
2024-10-16 16:54:16 +1100

    Implemented loading the next scene

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+
+    public void LoadNextScene(){
+        DontDestroyOnLoad(this);
+        SceneManager.LoadScene("Assignment3");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
